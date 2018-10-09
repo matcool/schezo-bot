e6a9538a4e9acd14374eab5c74215d75dd22328d
matcool <26722564+matcool@users.noreply.github.com>
2018-10-08 21:50:58 -0300

    handling missing arguments error

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -9,20 +9,23 @@ class ErrorHandlerCog:
         self.bot = bot
         self.noperm = bot.get_emoji(321784861595664385)
 
-    ignored = (commands.CommandNotFound, commands.UserInputError, commands.NotOwner)
+    ignored = (commands.CommandNotFound, commands.NotOwner)
     async def on_command_error(self,ctx,error):
         if isinstance(error,self.ignored):
             return
+
         
         if isinstance(error,discord.errors.Forbidden):
             try:
                 await ctx.message.add_reaction(self.noperm)
             except discord.errors.Forbidden:
                 return
+        elif isinstance(error,commands.MissingRequiredArgument):
+            await ctx.send('{} is a required argument that is missing.'.format(error.param))
         else:
             print('Ignoring exception in command {}:'.format(ctx.command))
-            #traceback.print_exception(type(error), error, error.__traceback__)
-            print(error)
+            traceback.print_exception(type(error), error, error.__traceback__)
+            #print(error)
             #print("error line {}".format(sys.exc_info()[-1].tb_lineno))
 
 def setup(bot):
