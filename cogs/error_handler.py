f8237f9f55dd13e561f29bfbcdf69b113b6f772a
matcool <26722564+matcool@users.noreply.github.com>
2019-02-13 18:06:22 -0300

    get closest command to unknown one

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -3,15 +3,35 @@ import traceback
 #import sys
 from discord.ext import commands
 
+#https://github.com/TheAlgorithms/Python/blob/master/strings/levenshtein_distance.py
+def string_distance(a, b):
+    if len(a) < len(b): return string_distance(b, a)
+    if len(b) == 0: return len(a)
+    prow = range(len(b) + 1)
+    for i, c1 in enumerate(a):
+        crow = [i + 1]
+        for j, c2 in enumerate(b):
+            ins = prow[j + 1] + 1
+            dl = crow[j] + 1
+            sub = prow[j] + (c1 != c2)
+            crow.append(min(ins, dl, sub))
+        prow = crow
+    return prow[-1]
 
 class ErrorHandlerCog:
     def __init__(self, bot):
         self.bot = bot
         self.noperm = bot.get_emoji(321784861595664385)
 
-    ignored = (commands.CommandNotFound, commands.NotOwner)
+    ignored = (commands.NotOwner)
     async def on_command_error(self,ctx,error):
         if isinstance(error,self.ignored):
+            return
+
+        if isinstance(error,commands.CommandNotFound):
+            cmds = list(map(lambda x: x.name, self.bot.commands))
+            cmds.sort(key=lambda x: string_distance(x,ctx.invoked_with))
+            await ctx.send(f'Unknown command! Did you mean {cmds[0]}?')
             return
 
         if isinstance(error,discord.errors.Forbidden):
