779d790857e6656b0ace9158689334e5ecab6b5a
matcool <26722564+matcool@users.noreply.github.com>
2019-02-24 09:32:01 -0300

    improved help command

--- cogs/custom_help.py
+++ cogs/custom_help.py
@@ -5,16 +5,21 @@ class customHelp(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(hidden=True)
-    async def help(self,ctx,cmdOrCog=None,type=None):
-        # Show all cogs and their commands WITHOUT short doc
-        if cmdOrCog is None:
-            cmds = {}
-            for cmd in self.bot.commands:
+    def commands_dict(self):
+        final = {}
+        for cogname, cog in self.bot.cogs.items():
+            for cmd in cog.get_commands():
                 if not cmd.hidden:
-                    if cmds.get(cmd.cog_name) is None:
-                        cmds[cmd.cog_name] = []
-                    cmds[cmd.cog_name].append(cmd)
+                    if final.get(cogname) == None:
+                        final[cogname] = []
+                    final[cogname].append(cmd)
+        return final
+
+    @commands.command(hidden=True)
+    async def help(self,ctx,lookup=None,type=None):
+        # Show all cogs and their commands WITHOUT short doc
+        if lookup is None:
+            cmds = self.commands_dict()
 
             # Sort by name
             for k in cmds:
@@ -30,7 +35,7 @@ class customHelp(commands.Cog):
 
         # Look up command or cog
         else:
-            cmd = self.bot.get_command(cmdOrCog)
+            cmd = self.bot.get_command(lookup)
             if cmd and type != 'cog':
                 if cmd.help:
                     embed = discord.Embed(
@@ -43,25 +48,21 @@ class customHelp(commands.Cog):
                     await ctx.send('No help found for that command (blame mat)')
                     return
             else:
-                cmds = {}
-                for cmd in self.bot.commands:
-                    if not cmd.hidden:
-                        if cmds.get(cmd.cog_name) is None:
-                            cmds[cmd.cog_name] = []
-                        cmds[cmd.cog_name].append(cmd)
+                cog = self.bot.get_cog(lookup)
+                if cog != None:
+                    cog = list(filter(lambda x: not x.hidden,cog.get_commands()))
 
-                if cmds.get(cmdOrCog) is None:
-                    await ctx.send('No command or category found with that name')
+                if cog == None or len(cog) == 0:
+                    await ctx.send('No command or cog found with that name')
+                    return
 
-                for k in cmds:
-                    cmds[k].sort(key = lambda c: not bool(c.help))
+                cog.sort(key = lambda c: not bool(c.help))
 
-                else:
-                    final = ""
-                    for cmd in cmds[cmdOrCog]:
-                        final += f"**{cmd.name}**" + (f" - {cmd.short_doc}" if cmd.short_doc else "") + "\n"
-                    embed = discord.Embed(title=cmdOrCog,description=final,colour=0x3498db)
-                    await ctx.send(embed=embed)
+                final = ""
+                for cmd in cog:
+                    final += f"**{cmd.name}**" + (f" - {cmd.short_doc}" if cmd.short_doc else "") + "\n"
+                embed = discord.Embed(title=lookup,description=final,colour=0x3498db)
+                await ctx.send(embed=embed)
 
 
 
