7d07d876ab7b8e73454b030a77811b4e3a6544de
matcool <26722564+matcool@users.noreply.github.com>
2019-10-07 20:13:35 -0300

    Private cog with kill command
    Currently only has the kill command but will be used to add hidden and/o
    owner only commands in the future

---
+++ cogs/private.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class Private(commands.Cog, command_attrs=dict(hidden=True)):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def kill(self, ctx):
+        try:
+            await ctx.message.add_reaction('🆗')
+        except discord.DiscordException:
+            pass
+        await self.bot.logout()
+
+def setup(bot):
+    bot.add_cog(Private(bot))
