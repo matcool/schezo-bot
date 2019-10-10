bd61fddd4df35bf2a1a079928eea6aeb79adae7e
matcool <26722564+matcool@users.noreply.github.com>
2019-10-10 18:19:55 -0300

    Slightly changed info command

--- cogs/general.py
+++ cogs/general.py
@@ -15,16 +15,14 @@ class General(commands.Cog):
         """Sends some info about the bot"""
         process = psutil.Process(os.getpid())
 
-        embed = discord.Embed(title='Schezo', description=f'''
-        [Source code](https://github.com/matcool/schezo-bot)
-        Memory usage: **{process.memory_info().rss/1024/1024:.2f}MB**''', colour=0x264a78)
+        embed = discord.Embed(title='Schezo', description=f'[Source code](https://github.com/matcool/schezo-bot)', colour=0x264a78)
 
         embed.set_thumbnail(url=await get_avatar(self.bot.user, url=True))
 
         embed.add_field(name='Uptime', value=format_time(int(self.bot.uptime)))
+        embed.add_field(name='Memory usage', value=f'{process.memory_info().rss/1024/1024:.2f}MB')
         embed.add_field(name='Guilds', value=len(self.bot.guilds))
         embed.add_field(name='Users', value=len(self.bot.users))
-        embed.add_field(name='Cached Messages', value=len(self.bot.cached_messages))
         await ctx.send(embed=embed)
 
 def setup(bot):
