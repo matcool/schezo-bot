c964e1a87683c3d52a47f484e5a5328335abb0b3
matcool <26722564+matcool@users.noreply.github.com>
2019-10-17 16:26:10 -0300

    Ping command

--- cogs/general.py
+++ cogs/general.py
@@ -1,10 +1,12 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
 import psutil
 import os
 import random
 from .utils.time import format_time
 from .utils.message import get_avatar
+import time
 
 class General(commands.Cog):
     def __init__(self, bot):
@@ -24,6 +26,16 @@ class General(commands.Cog):
         embed.add_field(name='Guilds', value=len(self.bot.guilds))
         embed.add_field(name='Users', value=len(self.bot.users))
         await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.cooldown(1, 2, BucketType.default)
+    async def ping(self, ctx):
+        """Sends bot's latency"""
+        start = time.time()
+        msg = await ctx.send(f'Websocket: {int(self.bot.latency*1000)}ms')
+        end = time.time()
+        await msg.edit(content=f'Websocket: {int(self.bot.latency*1000)}ms\n'
+                               f'Message: {int((end-start)*1000)}ms')
 
 def setup(bot):
     bot.add_cog(General(bot))
