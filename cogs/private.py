586b61e68be89567397a4afabcdb653640bb8592
matcool <26722564+matcool@users.noreply.github.com>
2019-10-20 10:20:34 -0300

    Eval command
    Highly based off [RoboDanny's eval](https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/admin.py#L216)
    
    Also removed reaction on bot kill as it was making the logout take longer

--- cogs/private.py
+++ cogs/private.py
@@ -1,6 +1,10 @@
 import discord
 from discord.ext import commands
 import pendulum
+import textwrap
+import io
+from contextlib import redirect_stdout
+import traceback
 
 class Private(commands.Cog, command_attrs=dict(hidden=True)):
     def __init__(self, bot):
@@ -9,10 +13,6 @@ class Private(commands.Cog, command_attrs=dict(hidden=True)):
     @commands.command()
     @commands.is_owner()
     async def kill(self, ctx):
-        try:
-            await ctx.message.add_reaction('🆗')
-        except discord.DiscordException:
-            pass
         self.bot.logger.info('Logging out')
         await self.bot.logout()
 
@@ -21,6 +21,54 @@ class Private(commands.Cog, command_attrs=dict(hidden=True)):
         """How many days left for mat to be free"""
         period = pendulum.date(2019, 11, 14) - pendulum.now()
         await ctx.send(f'{period.in_days()} days left!!!')
+
+    # This is highly based off https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/admin.py#L216
+    @commands.command(name='eval')
+    @commands.is_owner()
+    async def _eval(self, ctx, *, code):
+        code = code.splitlines()
+        # Trim codeblock
+        if code[0].startswith('```'):
+            code = code[1:-1]
+
+        # add return statement if eval is only one line
+        if len(code) == 1 and not code[0].startswith('return'):
+            code[0] = f'return {code[0]}'
+
+        # use 4 spaces as it's discord code blocks indent size
+        code = textwrap.indent('\n'.join(code), '    ')
+
+        # put code in async function
+        code = f"async def func():\n{code}"
+
+        env = {
+            'ctx': ctx,
+            'bot': self.bot
+        }
+        stdout = io.StringIO()
+
+        try:
+            exec(code, env)
+        except Exception as e:
+            # This will most likely be a syntax error
+            # since it was thrown before even running the function
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+        else:
+            func = env['func']
+            # run function and get return and stdout
+            try:
+                with redirect_stdout(stdout):
+                    ret = await func()
+            except Exception as e:
+                value = stdout.getvalue()
+                await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+            else:
+                value = stdout.getvalue()
+                # prefer stdout over function return value
+                if value:
+                    await ctx.send(f'```py\n{value}```')
+                elif ret is not None:
+                    await ctx.send(f'```py\n{repr(ret)}```')
 
 def setup(bot):
     bot.add_cog(Private(bot))
