db2c4c5e60258b59f7ac11d0adfcf556ca85f5b1
matcool <26722564+matcool@users.noreply.github.com>
2019-12-27 20:02:26 -0300

    skywars stats command

--- cogs/hypixel.py
+++ cogs/hypixel.py
@@ -36,6 +36,7 @@ class Hypixel(commands.Cog):
         if not data['success'] or not data['player']: return None
         return data['player']
 
+    @commands.cooldown(70, 60, BucketType.default)
     @commands.command()
     async def bwstats(self, ctx, username):
         uuid = await self.get_uuid(username)
@@ -63,7 +64,7 @@ class Hypixel(commands.Cog):
         coins = f'{coins:,}' # 10000 -> 10,000
 
         embed = discord.Embed(title=f"{name}'s Hypixel Bedwars stats", colour=0xff7575)
-        embed.set_thumbnail(url=f'https://minotar.net/helm/{name}/256.png')
+        embed.set_thumbnail(url=f'https://minotar.net/helm/{uuid}/256.png')
         
         embed.add_field(name='Bedwars Level',
                         value=f'{level}\N{WHITE MEDIUM STAR}', inline=False)
@@ -76,6 +77,48 @@ class Hypixel(commands.Cog):
 
         embed.add_field(name='Items',
                         value=f"{coins} Coins\n{loot_chests} Loot Chest{'s' if loot_chests > 1 else ''}", inline=True)
+
+        await ctx.send(embed=embed)
+
+    @commands.cooldown(70, 60, BucketType.default)
+    @commands.command()
+    async def swstats(self, ctx, username):
+        uuid = await self.get_uuid(username)
+        if not uuid: return await ctx.send('Player not found')
+        
+        player = await self.get_player(uuid)
+        if not player: return await ctx.send('Player not found')
+        
+        sw = player.get('stats', {}).get('SkyWars')
+        if not sw: return await ctx.send('No skywars stats found for that player')
+        
+        name = player['displayname']
+
+        solo_kills = sw.get('kills_solo', 0)
+        solo_wins = sw.get('wins_solo', 0)
+        team_kills = sw.get('kills_team', 0)
+        team_wins = sw.get('wins_team', 0)
+        souls = sw.get('souls', 0)
+        tokens = sw.get('cosmetic_tokens', 0)
+        loot_chests = sw.get('skywars_chests', 0)
+
+        coins = sw.get('coins', 0)
+        coins = f'{coins:,}' # 10000 -> 10,000
+        
+        embed = discord.Embed(title=f"{name}'s Hypixel Skywars stats", colour=0xf4e842)
+        embed.set_thumbnail(url=f'https://minotar.net/helm/{uuid}/256.png')
+        
+        embed.add_field(name='Solo',
+                        value=f'{solo_kills} Kills\n{solo_wins} Wins', inline=True)
+        
+        embed.add_field(name='Team',
+                        value=f'{team_kills} Kills\n{solo_wins} Wins', inline=True)
+        
+        embed.add_field(name='Items',
+                        value=f'{coins} Coins\n{souls} Souls', inline=True)
+        
+        embed.add_field(name='Items',
+                        value=f'{tokens} Tokens\n{loot_chests} Loot Chests', inline=True)
 
         await ctx.send(embed=embed)
 
