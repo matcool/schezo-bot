c75e8e815bbb13d169c5883807edb7c2a3cf4d3d
matcool <26722564+matcool@users.noreply.github.com>
2020-04-11 23:06:07 -0300

    add video thumbnail

--- cogs/utils/message.py
+++ cogs/utils/message.py
@@ -18,18 +18,22 @@ async def get_msg_image(message: discord.Message, url: bool=False) -> Union[byte
             url_str = embed.thumbnail.url or embed.image.url
             if url_str: return url_str if url else await get_page(url_str)
 
-async def get_msg_video(message: discord.Message, max_size: int=8000000, url: bool=False) -> Union[bytes, str]:
+async def get_msg_video(message: discord.Message, max_size: int=8000000, url: bool=False, thumb: bool=False) -> Union[bytes, str]:
     if message.attachments:
         for att in message.attachments:
             file_type = await get_file_type(att.url)
             if file_type and file_type.startswith('video/'):
-                return att.url if url else await att.read()
+                if url: return att.url
+                elif thumb: return att.proxy_url + '?format=jpeg'
+                else: return await att.read()
     if message.embeds:
         for embed in message.embeds:
             if embed.video.url:
                 size = await get_file_size(embed.video.url)
                 if size and size < max_size:
-                    return embed.video.url if url else await get_page(embed.video.url)
+                    if url: return embed.video.url
+                    elif thumb: return embed.video.proxy_url + '?format=jpeg'
+                    else: return await get_page(embed.video.url)
 
 async def get_nearest(ctx: commands.Context, limit: int=10, lookup: Callable[[discord.Message], Union[bytes, str]]=get_msg_image, **lookup_kwargs) -> Union[bytes, str]:
     look = await lookup(ctx.message, **lookup_kwargs)
@@ -40,7 +44,7 @@ async def get_nearest(ctx: commands.Context, limit: int=10, lookup: Callable[[di
             if look is not None: break
     return look
 
-async def message_embed(message: discord.Message, original: bool=True, color: int=0xa3a3a3, timestamp: bool=True, attachments: bool=True) -> discord.Embed:
+async def message_embed(message: discord.Message, original: bool=True, color: int=0xa3a3a3, timestamp: bool=True, attachments: bool=True, thumb: bool=True) -> discord.Embed:
     embed = discord.Embed(description=message.clean_content, colour=color)
     embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
     if original:
@@ -50,6 +54,10 @@ async def message_embed(message: discord.Message, original: bool=True, color: in
     url = await get_msg_image(message, url=True)
     if url:
         embed.set_image(url=url)
+    elif thumb:
+        url = await get_msg_video(message, thumb=True)
+        if url:
+            embed.set_image(url=url)
     if attachments and message.attachments:
         fancy = '\n'.join(f'- [{att.filename}]({att.url})' for att in message.attachments if att.url != url)
         if fancy: embed.add_field(name='Attachments', value=fancy)
